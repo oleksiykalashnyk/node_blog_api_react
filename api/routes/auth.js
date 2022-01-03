91e2e0a43fb15fbc24dd144d0daa408e43bea3da
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2022-01-03 21:44:48 +0100

    test

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const User = require("../models/User");
 const bcrypt = require('bcrypt');
 
-//REGISTER
+//REGISTER 1
 router.post('/register', async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(10);
